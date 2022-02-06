5559e8d0ca6a65592de530a78a770010703c3d83
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2022-02-06 23:13:30 +0200

    templating directory setup

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs')
+
 app.get('/user/:username', (req, res)=> {
 	let user = req.params.username;
 	res.render('index.ejs', {username : user});
