fc7d97ea369a36c1e1367dcbce3e49879f4b03d9
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2022-02-06 23:21:43 +0200

    template loop use

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+const path = require('path')
 
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'ejs')
 
-app.get('/user/:username', (req, res)=> {
-	let user = req.params.username;
-	res.render('index.ejs', {username : user});
+app.get('/questions', (req, res)=> {
+	let questions = [
+		{title: "What is Node.js", user : "Kadi", votes : "10"},
+		{title: "What is Express.js?", user : "Mike", votes : "8"}
+	]
+
+	res.render('index', {questions:questions});
 });
 
 app.listen(3000, ()=> {
