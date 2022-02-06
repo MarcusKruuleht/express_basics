1248a0a8cca3784d4038b6d7f9f90a84759d0ebb
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2022-02-06 23:55:37 +0200

    post request use

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ const app = express();
 
 const path = require('path')
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
-app.get('/questions', (req, res)=> {
-	let questions = [
-		{title: "What is Node.js", user : "Kadi", votes : "10"},
-		{title: "What is Express.js?", user : "Mike", votes : "8"}
-	]
-
-	res.render('index', {questions:questions});
+app.get('/login', (req, res)=> {
+	res.render('login');
 });
+
+app.post('/login', (req, res)=> {
+	let username = req.body.username;
+	let password = req.body.password;
+
+	if(username === 'user' && password === 'qwerty'){
+		res.redirect('/dashboard');
+	}
+});
+
+app.get('/dashboard', (req, res)=> {
+	res.render('dashboard');
+})
 
 app.listen(3000, ()=> {
 	console.log('Server started on http://localhost:3000');
